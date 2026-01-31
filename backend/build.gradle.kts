f73ab8fb9fa0e342cb9b2f2fcd54561b7420f881
lautihevia <lautarohevia@outlook.com>
2026-01-31 18:10:56 -0300

    actualizacion dependencias

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,6 +31,12 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
+	
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
